b9ee60a896ac0c42ae0c9dca1c361e1068c4ba9e
SirKentut <pierreharbin@gmail.com>
2024-09-19 21:08:20 +0900

    Configuring TailwindCSS Json and dependancies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import HomeCooksPage from './pages/HomeCooksPage'
+import NewUserPage from './pages/NewUserPage';
 
 function App() {
   return (
     <Router>
-      <div>Welcome to the main page</div>
+      <h1 className="text-3xl font-bold underline">Welcome to the landing page</h1>
       <div className="App">
         <Routes>
         <Route path="/homecooks" element={<HomeCooksPage />} />
+        <Route path="/users" element={<NewUserPage/>} />
         </Routes>
       </div>
     </Router>
