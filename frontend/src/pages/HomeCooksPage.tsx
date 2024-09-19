370e8252d90abd53b2697e50ab7af14db5064ceb
SirKentut <pierreharbin@gmail.com>
2024-09-19 20:28:38 +0900

    Added import statements to ensure that pages properly render

---
+++ frontend/src/pages/HomeCooksPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeCooksPage: React.FC = () => {
+  return (
+    <div>
+      Hello welcome to the Home Cooks Page
+    </div>
+  );
+};
+
+export default HomeCooksPage;
---
+++ frontend/src/pages/NewUserPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewUserPage:React.FC=()=>{
+return(
+    <div>Welcome to the New User Page</div>
+    );
+};
+
+export default NewUserPage;
