2df967ebc709f24a78cef3ccc0450049e2925737
SirKentut <pierreharbin@gmail.com>
2024-09-21 23:06:10 +0900

    Implementing Calendar Component to User Page

--- frontend/src/pages/NewUserPage.tsx
+++ frontend/src/pages/NewUserPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ShuffleHero from "components/ui/sufflehero";
+import { Calendar } from 'components/ui/calendar';
 
 const NewUserPage:React.FC=()=>{
 return(
     <div>
         <h1>Welcome to the New User Page</h1>
         <ShuffleHero/>
+        <Calendar/>
     </div>
     );
 };
