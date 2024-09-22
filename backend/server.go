1bbd5b4239ab6984c9621309f32f6c1e599b99f8
SirKentut <pierreharbin@gmail.com>
2024-09-22 15:34:02 +0900

    Implementing backend server routing

--- backend/server.go
+++ backend/server.go
@@ -1,16 +1,43 @@
 package main
 
 import (
- "fmt"
- "log"
- "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
- http.HandleFunc("/", handler)
- log.Fatal(http.ListenAndServe(":8080", nil))
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
- fmt.Fprint(w, "Hello, World!")
+var users []User
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/", homePage).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	users = append(users, newUser)
+	json.NewEncoder(w).Encode(newUser)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
 }
