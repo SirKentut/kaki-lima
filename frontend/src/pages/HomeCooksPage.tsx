2a5d8ccfcdb6bfa36cbe18ee90cb7dda906d5b91
SirKentut <pierreharbin@gmail.com>
2024-10-03 23:30:38 +0900

    Updated CSS of Nav Bar component

--- frontend/src/pages/HomeCooksPage.tsx
+++ frontend/src/pages/HomeCooksPage.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const HomeCooksPage: React.FC = () => {
   return (
-    <div>
-      Hello welcome to the Home Cooks Page
-    </div>
+    <div className="bg-blue-500 text-black text-center p-10">
+    <h1 className="text-4xl font-bold">Welcome to the homecooks page!</h1>
+    <p className="text-lg mt-4">This is a test to see if Tailwind is affecting the components.</p>
+  </div>
+
   );
 };
 
