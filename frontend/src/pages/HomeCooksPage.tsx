a8ef819dfa95a10ed98f3fc392c25d82ac345512
SirKentut <pierreharbin@gmail.com>
2024-10-05 23:57:09 +0900

    Added Shadcn input component

--- frontend/src/pages/HomeCooksPage.tsx
+++ frontend/src/pages/HomeCooksPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Input } from 'components/ui/input';
 
 const HomeCooksPage: React.FC = () => {
   return (
     <div className="bg-blue-500 text-black text-center p-10">
     <h1 className="text-4xl font-bold">Welcome to the homecooks page!</h1>
     <p className="text-lg mt-4">This is a test to see if Tailwind is affecting the components.</p>
+    <Input type="text" placeholder='Text'/>
   </div>
+
+  
 
   );
 };
