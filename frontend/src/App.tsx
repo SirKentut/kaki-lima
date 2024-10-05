1f1f6fb24af3f7e0ed16e008fb0174430a929f0f
SirKentut <pierreharbin@gmail.com>
2024-10-06 00:58:30 +0900

    Refactor: Updated Folder paths for homecooks and users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomeCooksPage from './pages/HomeCooksPage';
-import NewUserPage from './pages/NewUserPage';
+import HomeCooksPage from './pages/homecooks/HomeCooksPage';
+import NewUserPage from './pages/users/NewUserPage';
 import { Checkbox } from 'components/ui/checkbox';
 import { UserNav} from 'components/ui/UserNav';
 import { User } from 'lucide-react';
