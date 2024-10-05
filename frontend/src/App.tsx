4530d0c4ac53c50a8ef794e35d4006b4ebb52699
SirKentut <pierreharbin@gmail.com>
2024-10-06 00:54:46 +0900

    Reconfig: Removed redundancy in App.tsx and tested userNav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeCooksPage from './pages/HomeCooksPage';
 import NewUserPage from './pages/NewUserPage';
 import { Checkbox } from 'components/ui/checkbox';
-
-
+import { UserNav} from 'components/ui/UserNav';
+import { User } from 'lucide-react';
 
 
 // Main App Component
 function App() {
   return (
     <Router>
-      <div className="App">
         {/* Routes */}
+        <UserNav />
         <Routes>
           <Route path="/homecooks" element={<HomeCooksPage />} />
           <Route path="/" element={<NewUserPage />} />
         </Routes>
-      </div>
     </Router>
   );
 }
--- frontend/src/components/ui/UserNav.tsx
+++ frontend/src/components/ui/UserNav.tsx
@@ -12,12 +12,63 @@ import{
     ListItem}
     from 'components/ui/NavigationMenu';
 
+
+
+    const components: { title: string; href: string; description: string }[] = [
+        {
+          title: "Browse",
+          href: "/",
+          description:
+            "Button that takes user to featured menus page",
+        },
+        {
+          title: "Hover Card",
+          href: "/docs/primitives/hover-card",
+          description:
+            "For sighted users to preview content available behind a link.",
+        },
+        {
+          title: "Progress",
+          href: "/docs/primitives/progress",
+          description:
+            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+        },
+        {
+          title: "Scroll-area",
+          href: "/docs/primitives/scroll-area",
+          description: "Visually or semantically separates content.",
+        },
+        {
+          title: "Tabs",
+          href: "/docs/primitives/tabs",
+          description:
+            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+        },
+        {
+          title: "Tooltip",
+          href: "/docs/primitives/tooltip",
+          description:
+            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+        },
+      ]
+
+//Todo - Add UserNav component
 function UserNav(){
-return{
-    
-    
-}
-}
+    return(
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <a>Browse</a>
+                    <NavigationMenuContent>
+                        <NavigationMenuLink>Link</NavigationMenuLink>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+
+        
+        );
+    }
 
 export {
 UserNav
