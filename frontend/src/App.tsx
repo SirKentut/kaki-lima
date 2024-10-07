b530487b26c3001130d587874412254a4f39e831
SirKentut <pierreharbin@gmail.com>
2024-10-07 15:34:18 +0900

    Explore Page: Added  an  explore page and refactored routing paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeCooksPage from './pages/homecooks/HomeCooksPage';
 import NewUserPage from './pages/users/NewUserPage';
+import UserExplorePage from './pages/users/UserExplorePage';
 import HomeCooksMenu from './pages/homecooks/HomeCooksMenu';
 import { Checkbox } from 'components/ui/checkbox';
 import { UserNav} from 'components/ui/UserNav';
@@ -16,8 +17,9 @@ function App() {
         <UserNav />
         <Routes>
           <Route path="/homecooks" element={<HomeCooksPage />} />
-          <Route path="/" element={<NewUserPage />} />
           <Route path="/homecooks/menu" element={<HomeCooksMenu />} />
+          <Route path="/" element={<NewUserPage />} />
+          <Route path="explore" element={<UserExplorePage />} />
         </Routes>
     </Router>
   );
---
+++ frontend/src/pages/users/UserExplorePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserExplorePage: React.FC = () => {
+  return (
+    <div className="bg-blue-500 text-black text-center p-10">
+    <h1 className="text-4xl font-bold">Explore! </h1>
+  </div>
+  );
+};
+
+export default UserExplorePage;
