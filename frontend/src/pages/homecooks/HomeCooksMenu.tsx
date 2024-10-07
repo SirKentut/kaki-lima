9815f6ca3ae08c2c8eed5742e9e1aad065a9ca4b
SirKentut <pierreharbin@gmail.com>
2024-10-07 15:50:17 +0900

    Chef Menu: Created Menu Component

--- frontend/src/pages/homecooks/HomeCooksMenu.tsx
+++ frontend/src/pages/homecooks/HomeCooksMenu.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import chefMenu from 'components/ui/chefMenu';
 
 const HomeCooksMenu: React.FC = () => {
     return (
     <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">create your awesome menu here</h1>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Create your awesome menu here</h1>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at Kaki Lima we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
             <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                 <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
@@ -17,6 +18,9 @@ const HomeCooksMenu: React.FC = () => {
                     Learn more
                 </a>  
             </div>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+            {chefMenu()}
         </div>
     </section>
 
