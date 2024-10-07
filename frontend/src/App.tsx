aa7e2a95c7e7b4f67c069433a5dc4651bee2b5bb
SirKentut <pierreharbin@gmail.com>
2024-10-07 14:35:47 +0900

    initialize menu page for HomeCooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeCooksPage from './pages/homecooks/HomeCooksPage';
 import NewUserPage from './pages/users/NewUserPage';
+import HomeCooksMenu from './pages/homecooks/HomeCooksMenu';
 import { Checkbox } from 'components/ui/checkbox';
 import { UserNav} from 'components/ui/UserNav';
 import { User } from 'lucide-react';
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/homecooks" element={<HomeCooksPage />} />
           <Route path="/" element={<NewUserPage />} />
+          <Route path="/homecooks/menu" element={<HomeCooksMenu />} />
         </Routes>
     </Router>
   );
---
+++ frontend/src/pages/homecooks/HomeCooksMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomeCooksMenu: React.FC = () => {
+    return (
+        <div className='bg-blue-500 text-black text-center p-10'>
+            <h1 className='text-4xl font-bold' text-center>Menu</h1>
+        </div>
+
+       
+    );
+
+};
+
+export default HomeCooksMenu;
