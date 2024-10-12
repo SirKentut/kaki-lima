cb8672df14c6869b8f2912166dae25cf654dcebc
SirKentut <pierreharbin@gmail.com>
2024-10-12 23:58:36 +0900

    Add: Accordion componenent to HomeCook page

--- frontend/src/pages/homecooks/HomeCooksPage.tsx
+++ frontend/src/pages/homecooks/HomeCooksPage.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Input } from 'components/ui/input';
 import  footer  from 'components/ui/footer';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "components/ui/accordion"
+
 
 const HomeCooksPage: React.FC = () => {
   return (
@@ -8,6 +15,14 @@ const HomeCooksPage: React.FC = () => {
     <h1 className="text-4xl font-bold">Welcome to the homecooks page!</h1>
     <p className="text-lg mt-4">This is a test to see if Tailwind is affecting the components.</p>
     <Input type="text" placeholder='Text'/>
+    <div>
+      <p className="text-lg mt-4">FAQ</p>
+      <Accordion type="single" collapsible>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>How can I get my store recognized</AccordionTrigger>
+        </AccordionItem>
+      </Accordion>
+    </div>
     <div>
       {footer()}
     </div>
