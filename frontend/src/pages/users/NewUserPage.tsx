bd1d87d45410270542e96d4bc51cd28925e013c9
SirKentut <pierreharbin@gmail.com>
2024-10-19 00:13:58 +0900

    Cleaning: User Landing Page

--- frontend/src/pages/users/NewUserPage.tsx
+++ frontend/src/pages/users/NewUserPage.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ShuffleHero from "components/ui/sufflehero";
-import { Calendar } from 'components/ui/calendar';
 
 const NewUserPage:React.FC=()=>{
 return(
     <div>
-        <h1>Welcome to Kaki Lima</h1>
         <ShuffleHero/>
-        <Calendar/>
     </div>
     );
 };
