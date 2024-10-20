411af0f876219925a1abb820c5426f5d6f4107c7
SirKentut <pierreharbin@gmail.com>
2024-10-21 00:30:23 +0900

    Install: material tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
 const { fontFamily } = require("tailwindcss/defaultTheme")
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   darkMode: ["class"],
   content: ["src/**/*.{ts,tsx,js,jsx}", "components/**/*.{ts,tsx,js,jsx}"],
   theme: {
@@ -73,4 +74,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+});
