a8998bff13b396205f2dceb299f76c8b44fdce65
SirKentut <pierreharbin@gmail.com>
2024-10-20 23:45:54 +0900

    Remove: Redundancy of import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,14 @@ import HomeCooksPage from './pages/homecooks/HomeCooksPage';
 import NewUserPage from './pages/users/NewUserPage';
 import UserExplorePage from './pages/users/UserExplorePage';
 import HomeCooksMenu from './pages/homecooks/HomeCooksMenu';
-import { Checkbox } from '@components/ui/checkbox';
 import { User } from 'lucide-react';
 
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 // Main App Component
 function App() {
