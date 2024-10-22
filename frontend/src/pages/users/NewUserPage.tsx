4f280936ac2376ef44930e5eb42d37dc784b53eb
SirKentut <pierreharbin@gmail.com>
2024-10-22 23:59:13 +0900

    Testing: Tailwind

--- frontend/src/pages/users/NewUserPage.tsx
+++ frontend/src/pages/users/NewUserPage.tsx
@@ -5,6 +5,15 @@ const NewUserPage:React.FC=()=>{
 return(
     <div>
         <ShuffleHero/>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center gap-x-4">
+  <div className="shrink-0">
+    <img className="size-12" src="/img/logo.svg" alt="ChitChat Logo"/>
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black">ChitChat</div>
+    <p className="text-slate-500">You have a new message!</p>
+  </div>
+</div>
     </div>
     );
 };
